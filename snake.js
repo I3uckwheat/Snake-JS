65a9d96778207bd122ad79652bfb073c017921a0
Briggs Elsperger <I3uckwheat@gmail.com>
2017-12-09 12:45:20 -0600

    renders playArea

--- snake.js
+++ snake.js
@@ -1,10 +1,41 @@
+const snakeGridSize = 40;
+
+const playArea = playAreaFactory(snakeGridSize);
+render(playArea);
+
 function playAreaFactory(size){
   var board = {}
   for(let i = 0; i < size; i++){
-    board[`row${i}`] = {}
+    board[`row${i}`] = []
     for(let j = 0; j < size; j++){
-      board[`row${i}`][`column${j}`] = " ";
+      board[`row${i}`][j] = " ";
     }
   }
+
+  board.gridSize = size;
   return board;
+}
+
+
+function generatePlayAreaContainer(playArea){
+  var playAreaContainer = document.createElement('div');
+  playAreaContainer.classList.add("playAreaContainer");
+
+  for(let i = 0; i < playArea.gridSize; i++) {
+    let rowContainer = document.createElement('div');
+    rowContainer.classList.add(`row${i}`, "row")
+
+    playArea[`row${i}`].forEach(function(space, index) {
+      let element = document.createElement('div');
+      element.classList.add(`column${index}`, "column")
+      element.textContent = space;
+      rowContainer.appendChild(element);
+    });
+    playAreaContainer.appendChild(rowContainer);
+  }
+  return playAreaContainer;
+}
+
+function render(playArea){
+  document.body.appendChild(generatePlayAreaContainer(playArea));
 }
