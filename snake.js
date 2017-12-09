0f8368ec12f6aa69c7a99fe2a67403770692f550
Briggs Elsperger <I3uckwheat@gmail.com>
2017-12-09 11:58:02 -0600

    creates playArea

--- snake.js
+++ snake.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+function playAreaFactory(size){
+  var board = {}
+  for(let i = 0; i < size; i++){
+    board[`row${i}`] = {}
+    for(let j = 0; j < size; j++){
+      board[`row${i}`][`column${j}`] = " ";
+    }
+  }
+  return board;
+}
